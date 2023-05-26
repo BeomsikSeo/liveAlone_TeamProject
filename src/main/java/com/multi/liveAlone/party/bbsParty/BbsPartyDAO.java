469d310e78ffe0149dcc7beaf425f6922d4101a3
choidongheon <ehdgjs855@gmail.com>
2023-05-26 17:03:01 +0900

    Revert "5/26일자"
    This reverts commit 2c29039305f39bff087d3d37cdb83d07cecc3005.

--- src/main/java/com/multi/liveAlone/party/bbsParty/BbsPartyDAO.java
+++ src/main/java/com/multi/liveAlone/party/bbsParty/BbsPartyDAO.java
@@ -1,45 +1,5 @@
 package com.multi.liveAlone.party.bbsParty;
 
-import java.util.List;
-
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
 public class BbsPartyDAO {
-	@Autowired
-	SqlSessionTemplate my;
-
-
-	public List<BbsPartyVO> all(PagePartyVO vo) {
-		return my.selectList("bbsParty.all", vo);
-	}
-
-	public List<ChatPartyVO> chatAll(int partyBbs_num) {
-		/*
-		 * ChatPartyVO vo = new ChatPartyVO(); vo.setPartyBbs_num(partyBbs_num);
-		 */
-		return my.selectList("bbsParty.chatAll", partyBbs_num);
-	}
-	
-	public int count() {
-		return my.selectOne("bbsParty.count");
-	}                                                               
-	public BbsPartyVO one(BbsPartyVO vo) {
-		return my.selectOne("bbsParty.one", vo);
-	}                                                               
-	public int insert(BbsPartyVO vo) {
-		return my.insert("bbsParty.in", vo);
-	}
-	public int insert2(MemberPartyVO vo) {
-		return my.insert("bbsParty.in2", vo);
-	}
-	public int up(BbsPartyVO vo) {
-		return my.update("bbsParty.up", vo);
-	}
-	public int del(BbsPartyVO vo) {
-		return my.delete("bbsParty.del", vo);
-	}
 
 }
